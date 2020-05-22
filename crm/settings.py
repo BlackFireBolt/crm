24244ebfe7fd447ca820b0cb7601f151602ae8d1
BlackFireBolt <boltward@gmail.com>
2020-05-22 17:36:24 +0300

    version 0.7

--- crm/settings.py
+++ crm/settings.py
@@ -83,14 +83,21 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'crm_db',
+#        'USER' : 'crm_admin',
+#        'PASSWORD' : 'root',
+#        'HOST' : '127.0.0.1',
+#        'PORT' : '5432',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'crm_db',
-        'USER' : 'crm_admin',
-        'PASSWORD' : 'root',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'repo-db.sqlite3'),
     }
 }
 
